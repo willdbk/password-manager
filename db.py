f7fdc469c0e72d40a3fd4c3956223aad0eae715b
Will deBruynKops <willdbk@gmail.com>
2018-11-27 23:17:17 +0100

    begin to implement profile DB's

--- db.py
+++ db.py
@@ -1,23 +1,33 @@
 import sqlite3
+import random
 
 class Database:
 
     def __init__(self):
-        self.db_file = 'password_manager.sqlite'
+        self.db_file = 'profiles.sqlite'
         self.table_name = 'users'
-        self.column_name = 'profile_name_hash'
+        self.column_name_hash = 'profile_name_hash'
         self.column_salt = 'profile_salt'
         self.column_auth_key = 'authentication_key'
-        self.field_type = 'TEXT' # CHANGE BACK TO BLOB!!!
+        self.column_file_ref = 'profile_file_ref' # Reference to account sql file for each account
+        self.field_type = 'TEXT' # CHANGE BACK TO BLOB!!! (or do we have to?)
+        # Column names for account databases
+
+        # Account has 5 fields: hash(URL), hash(username), salt, nonce, enc(pwd)
+        self.prof_table_name = 'accounts'
+        self.column_prof_URL_hash = 'prof_url_hash'
+        self.column_prof_username_hash = 'prof_username_hash'
+        self.column_prof_salt = 'prof_salt'
+        self.column_prof_nonce = 'prof_nonce'
+        self.column_prof_pwd_enc = 'prof_pwd_enc'
 
         self.conn = sqlite3.connect(self.db_file)
         c = self.conn.cursor()
 
-        # creating a new SQLlite table with 1 column and set it as the PRIMARY
-        #  KEY
+        # creating a new SQLlite table with 1 column and set it as the PRIMARY KEY
         try:
             c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
-            .format(tn=self.table_name, nf=self.column_name, ft=self.field_type))
+            .format(tn=self.table_name, nf=self.column_name_hash, ft=self.field_type))
 
             # adding new column for salt values
             c.execute("ALTER TABLE {tn} ADD COLUMN {cn} {ct}"\
@@ -26,6 +36,11 @@ class Database:
             # adding a new column for authentication key
             c.execute("ALTER TABLE {tn} ADD COLUMN {cn} {ct}"\
             .format(tn=self.table_name, cn=self.column_auth_key, ct=self.field_type))
+
+            # adding file reference
+            c.execute("ALTER TABLE {tn} ADD COLUMN {cn} {ct}"\
+            .format(tn=self.table_name, cn=self.column_file_ref, ct=self.field_type))
+            print("table created")
         except:
             print("table already exists")
 
@@ -42,17 +57,93 @@ class Database:
     def add_profile(self, name_hash, salt, auth_key):
         c = self.conn.cursor()
         try:
-            c.execute("INSERT INTO users (profile_name_hash, profile_salt, authentication_key) VALUES (?, ?, ?)", (name_hash, salt, auth_key))
+            prof_file_ref_name = str(random.randrange(100000000, 999999999))+'.sqlite'
+            self.create_profile_db(prof_file_ref_name)
+            c.execute("INSERT INTO users (profile_name_hash, profile_salt, authentication_key, profile_file_ref) VALUES (?, ?, ?, ?)", (name_hash, salt, auth_key, prof_file_ref_name))
             self.conn.commit()
             return True
         except sqlite3.IntegrityError:
             print('ERROR: ID already exists')
             return False
 
+    def set_active_profile(self,name_hash):
+        profile_name = get_account_ref(self,name_hash)
+        self.profile_conn = sqlite3.connect(profile_name)
+
+# retrieval functions
+    def get_value_from_account(self,name_hash,column_name):
+        if(self.exists(name_hash)):
+            c = self.conn.cursor()
+            c.execute('SELECT {cw} FROM {tn} WHERE {cn}="{nh}"'.\
+            format(cw=column_name,tn=self.table_name,cn=self.column_name_hash,nh=name_hash))
+            return c.fetchone() #is this correct? (does not work when there are multiple accounts with the same prof ref... which there should never be)
+        else:
+            raise ValueError('ERROR: no value for column' + column_name + " found under account hash "+name_hash)
+
+    def get_salt(self, name_hash):
+        return self.get_value_from_account(name_hash,self.column_salt)
+
+    def get_authkey(self, name_hash):
+        return self.get_value_from_account(name_hash,self.column_auth_key)
+
+    def get_account_ref(self, name_hash):
+        return self.get_value_from_account(name_hash,self.column_file_ref)
+
     def exists(self, name_hash):
+        c = self.conn.cursor()
         try:
             c.execute('SELECT * FROM {tn} WHERE {cn}="{nh}"'.\
-            format(tn=self.table_name, cn=self.column_name, nh=name_hash))
+            format(tn=self.table_name, cn=self.column_name_hash, nh=name_hash))
+            if(c.fetchone() == None):
+                return False
             return True
         except:
             return False
+
+# account db functions
+    def create_profile_db(self, prof_file_ref_name):
+        self.profile_conn = sqlite3.connect(prof_file_ref_name)
+        c = self.profile_conn.cursor()
+
+        # creating a new SQLlite table with 1 column and set it as the PRIMARY KEY
+        try:
+            # adding new column for URL hash
+            c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
+            .format(tn=self.prof_table_name, nf=self.column_prof_URL_hash, ft=self.field_type))
+
+            # adding new column for username hash
+            c.execute("ALTER TABLE {tn} ADD COLUMN {cn} {ct}"\
+            .format(tn=self.prof_table_name, cn=self.column_prof_username_hash, ct=self.field_type))
+
+            # adding new column for salt
+            c.execute("ALTER TABLE {tn} ADD COLUMN {cn} {ct}"\
+            .format(tn=self.prof_table_name, cn=self.column_prof_salt, ct=self.field_type))
+
+            # adding new column for nonce
+            c.execute("ALTER TABLE {tn} ADD COLUMN {cn} {ct}"\
+            .format(tn=self.prof_table_name, cn=self.column_prof_nonce, ct=self.field_type))
+
+            # adding new column for enc(pwd)
+            c.execute("ALTER TABLE {tn} ADD COLUMN {cn} {ct}"\
+            .format(tn=self.prof_table_name, cn=self.column_prof_pwd_enc, ct=self.field_type))
+
+            print("table created")
+        except:
+            print("table already exists")
+
+        # committing changes and closing the connection
+        self.conn.commit()
+
+    def add_account(self, URL_hash, username_hash, salt, nonce, pwd_enc):
+        c = self.profile_conn.cursor()
+        try:
+            c.execute("INSERT INTO accounts (prof_url_hash, prof_username_hash, prof_salt, prof_nonce, prof_pwd_enc) VALUES (?, ?, ?, ?, ?)", (URL_hash, username_hash, salt, nonce, pwd_enc))
+            self.profile_conn.commit()
+            return True
+        except sqlite3.IntegrityError:
+            print('ERROR: account already exists')
+            return False
+
+    ''''def find_account_enc_pwd(self, URL_hash, username_hash):
+        c = self
+    def find_account_salt(self, URL_hash, username_hash)'''
--- main.py
+++ main.py
@@ -1,15 +1,81 @@
 import pyperclip
 import random
 import getpass
-import profile
+import sqlite3
+import sys, getopt, random
+from base64 import b64encode, b64decode
+from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Random import get_random_bytes
+from Crypto.Util.strxor import strxor
+import db
 
+profile_name_hash = ""
+database = db.Database()
 
 def get_random_password():
     s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
     passlen = 8
     p =  "".join(random.sample(s,passlen)) # cryptographically secure???
     return p
+
+def add_profile(pwd):
+    salt = get_random_bytes(8)
+    authkey = PBKDF2(pwd, salt)
+    # database.add_profile(profile_name_hash,salt,authkey)
+    database.add_profile(profile_name_hash,"salt","authkey")
+
+
+def profile_exists():
+    return database.exists(profile_name_hash)
+
+def print_database():
+    database.print_all()
+
+def authenticate(pwd):
+    salt = database.get_salt(profile_name_hash)[0]
+    print(salt)
+    authkey = PBKDF2(pwd, salt)
+    print(database.get_authkey(profile_name_hash)[0])
+    if(database.get_authkey(profile_name_hash) == authkey):
+        return True;
+    return True;
+
+# Account has 5 fields: hash(URL), hash(username), salt, nonce, enc(pwd)
+# the pwd is encrypted with a key generated from PBKDF2 using the salt and the master_password
+# then pwd is then encrypted in CTR mode with the nonce
+def add_account(self, URL, username, pwd, master_password):
+    #these references to the database aren't right
+    URL_hash = URL
+    username_hash = username
+    salt = get_random_bytes(8)
+    nonce = get_random_bytes(8)
+
+    enc_key = PBKDF2(master_password, salt)
+    # create a counter object and set the nonce as its prefix and set the initial counter value to 0
+    ctr = Counter.new(64, prefix=nonce, initial_value=0)
+    cipher = AES.new(enc_key, AES.MODE_CTR, counter=ctr)
+
+    # encrypt the plaintext
+    enc_pwd = cipher.encrypt(plaintext)
+    database.add_account(URL_hash, username_hash, salt, nonce, enc_pwd)
+
+
+def retrieve_pwd(self, URL, username, master_password):
+    #these references to the database aren't right
+    account = db.get_account_with_url_and_username()
+    salt = account["salt"]
+    nonce = account["nonce"]
+    enc_pwd = account["enc_pwd"]
+
+
+    dec_key = PBKDF2(master_password, salt)
+    # create a counter object and set the nonce as its prefix and set the initial counter value to 0
+    ctr = Counter.new(64, prefix=nonce, initial_value=0)
+    cipher = AES.new(enc_key, AES.MODE_CTR, counter=ctr)
+
+    # encrypt the plaintext
+    dec_pwd = cipher.decrypt(plaintext)
+    return dec_pwd
 
 
 
@@ -25,28 +91,35 @@ except FileNotFoundError:
     #insert code for initializing DB for first time startup
 '''
 
+print_database()
+
+
 profile_name = input("Enter your profile name: ")
-profile = profile.Profile(profile_name)
+profile_name_hash = profile_name
 
-profile.print_all()
 
-if(not profile.exists()):
+if(not profile_exists()):
     print("A Profile with given username does not exist.")
     answer = input("Would you like to create a password management profile? (y or n)\n")
     if('y' in answer):
         password = getpass.getpass("Enter the MASTER password for this profile: ")
-        profile.add_profile(password)
+        add_profile(password)
         password = get_random_bytes(len(password))
     else:
         sys.exit(2)
 else:
-    password = input("Enter your master password:")
-    # authenticated = user.authenticate(password)
+    password = getpass.getpass("Enter your MASTER password: ")
+    authenticated = authenticate(password)
     password = get_random_bytes(len(password))
     if(not authenticated):
         sys.exit(2)
+    database.set_active_account(profile_name_hash)
 
-response = input("Would you like to add data for an account ('a') or retrieve a saved password ('r')?\n")
+print()
+print("You are INNN")
+print()
+
+response = input("Would you like to add data for an account ('a') or retrieve a saved password ('r')? ")
 while(response != "exit"):
     if(response == 'a'):
         url = input("What is the URL for this account?")
